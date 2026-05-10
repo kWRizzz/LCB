d0adfbd7e787943bcd9720dd85630b84bad39709
kWRizzz <krishnathey2j@gmail.com>
2026-05-11 00:05:06 +0530

    router cloudinary done

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,25 @@ import express from "express";
 import cookieparser from "cookie-parser";
 import cors from "cors"
 import dotenv from "dotenv"
+import userRouter from "./src/routes/auth.route.js"
+
 
 dotenv.config()
 
 const app = express()
+const PORT= process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors)
 app.use(cookieparser())
 
+app.use("/api/user", userRouter)
+
+
+
 app.post("",(req,res)=>{
     res.cookie
 })
 
-app.listen(3000)
+app.listen(PORT)
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,15 @@
 import express from "express";
+import { registerUser,loginUser,logoutUser } from "../controller/auth.controller.js";
 
 const router = express.Router()
 
 
+router.post("/login",loginUser)
+
+
+router.post('/register',registerUser)
+
+router.post('/logout',logoutUser)
+
+
+export default router
